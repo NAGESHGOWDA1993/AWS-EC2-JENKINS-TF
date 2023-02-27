3e887aca9ae0c73b8e65ee9f2a19d8e4b0544828
Nagesha ks <109476085+NAGESHGOWDA1993@users.noreply.github.com>
2023-02-27 00:50:03 +0000

    Update variables.tf
    Updated aws region

--- variables.tf
+++ variables.tf
@@ -1,13 +1,12 @@
 variable "region" {
-  default = "us-east-1"
+  default = "eu-west-1"
 }
 
 variable "ami_id" {
   type = "map"
 
   default = {
-    us-east-1    = "ami-035b3c7efe6d061d5"
-    eu-west-2    = "ami-132b3c7efe6sdfdsfd"
-    eu-central-1 = "ami-9787h5h6nsn"
+    eu-west-1    = "ami-06e0ce9d3339cb039"
+    
   }
 }
