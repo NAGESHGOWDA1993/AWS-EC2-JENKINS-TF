dbfde69c5f8733d1e68d88c35f6729d1028d3087
Nagesha ks <109476085+NAGESHGOWDA1993@users.noreply.github.com>
2023-03-01 00:10:31 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "us-east-2"
+  default     = "eu-west-1"
 }
 
 variable "key_name" {
@@ -25,5 +25,5 @@ variable "tag_name" {
 
 variable "ami_id" {
   description = "AMI for Amazon Ami2 Ec2 instance"
-  default     = "ami-002068ed284fb165b"
+  default     = "ami-065793e81b1869261"
 }
