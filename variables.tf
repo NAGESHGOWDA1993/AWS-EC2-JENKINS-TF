5b1c952a3acfcf7185fffb1e7deccb328076fc9e
Nagesha ks <109476085+NAGESHGOWDA1993@users.noreply.github.com>
2023-03-03 15:11:54 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "Ec2keypair"
+  default     =  "kmaster"
 }
 
 variable "instance_type" {
